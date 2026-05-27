44d2cf334e957accd18798250d9bf79ad3dcee47
taynasantana <70611673+taynasantana@users.noreply.github.com>
2026-05-27 20:30:07 -0300

    Update test_de_caso_de_uso_04_java_single_file.java

--- test_de_caso_de_uso_04_java_single_file.java
+++ test_de_caso_de_uso_04_java_single_file.java
@@ -1,1 +1,21 @@
+// tests/usecase/stop_simulator_usecase_test.go
+package usecase_test
 
+import (
+	"os/exec"
+	"testing"
+)
+
+func TestCasoDeUsoPararSimulador(t *testing.T) {
+
+	cmd := exec.Command(
+		"./simulador",
+		"stop",
+	)
+
+	err := cmd.Run()
+
+	if err != nil {
+		t.Fatal("Falha no caso de uso de parar simulador")
+	}
+}
