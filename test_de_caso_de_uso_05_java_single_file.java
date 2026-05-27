f6616f13af282dad83af7a4bfd86d7678899691f
taynasantana <70611673+taynasantana@users.noreply.github.com>
2026-05-27 20:30:33 -0300

    Update test_de_caso_de_uso_05_java_single_file.java

--- test_de_caso_de_uso_05_java_single_file.java
+++ test_de_caso_de_uso_05_java_single_file.java
@@ -1,1 +1,21 @@
+// tests/usecase/status_simulator_usecase_test.go
+package usecase_test
 
+import (
+	"os/exec"
+	"testing"
+)
+
+func TestCasoDeUsoStatusSimulador(t *testing.T) {
+
+	cmd := exec.Command(
+		"./simulador",
+		"status",
+	)
+
+	err := cmd.Run()
+
+	if err != nil {
+		t.Fatal("Falha no caso de uso de status do simulador")
+	}
+}
