3afddea3fec889cc460a33e8e88e6d70c9c5e96e
taynasantana <70611673+taynasantana@users.noreply.github.com>
2026-05-27 20:29:11 -0300

    Update test_de_caso_de_uso_03_java_single_file.java

--- test_de_caso_de_uso_03_java_single_file.java
+++ test_de_caso_de_uso_03_java_single_file.java
@@ -1,1 +1,21 @@
+// tests/usecase/start_simulator_usecase_test.go
+package usecase_test
 
+import (
+	"os/exec"
+	"testing"
+)
+
+func TestCasoDeUsoIniciarSimulador(t *testing.T) {
+
+	cmd := exec.Command(
+		"./simulador",
+		"start",
+	)
+
+	err := cmd.Run()
+
+	if err != nil {
+		t.Fatal("Falha no caso de uso de iniciar simulador")
+	}
+}
